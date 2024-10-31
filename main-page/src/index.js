60f0ba767221961b1e30a30bbd6e66d1ecaac136
Matthew <olsonm811@gmail.com>
2024-10-31 18:38:13 -0400

    ready to deploy

---
+++ main-page/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './css/index.css';
+
+<BrowserRouter basename={ProcessingInstruction.env.PUBLIC_URL}>
+
+</BrowserRouter>
